1134f03d3a3b196e255ffe3a6f33a7608b62b83d
jorism1993 <jorismollinga@hotmail.com>
2018-01-23 16:34:05 +0100

    joris

--- HW2/scratch.py
+++
@@ -1,4 +0,0 @@
-from subprocess import Popen, PIPE
-process = Popen(['cd trec_eval','./trec_eval -m map -m P.5 -m ndcg -m recall.1000 qrel_test JelinekMercer.run'],stdout = PIPE, shell=True)
-line = process.stdout.readline()
-print (line)
