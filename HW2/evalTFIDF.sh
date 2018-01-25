9b3d44a3b4e42928aa5f6bd722ac1d26a5ae7e6e
Stefan Knegt <stefanknegt@gmail.com>
2018-01-25 16:19:38 +0100

    Cleaned repo

--- HW2/evalTFIDF.sh
+++
@@ -1,2 +0,0 @@
-cd trec_eval
-./trec_eval -m map -m P.5 -m ndcg_cut.10 -m recall.1000 qrel_validation TFIDF-joris.run
--- HW2/plm.sh
+++
@@ -1,2 +0,0 @@
-cd trec_eval
-./trec_eval -m ndcg_cut.10 -q qrel_validation plm.run
